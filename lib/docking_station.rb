512b6fc62af808f982cf1231474d998f65600b6b
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-08-30 16:37:22 +0100

    Limiting Capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,10 +3,12 @@ require_relative 'bike'
 class DockingStation
 
   def release_bike
-    Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
+    fail 'Dock full' if @bike
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,18 +3,38 @@ require './lib/docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
+
+  it 'raises an error when there is already a bike docked' do
+    subject.dock(Bike.new)
+    expect { subject.dock Bike.new }.to raise_error 'Dock full'
+  end
+
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
 end
