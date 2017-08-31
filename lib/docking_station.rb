a3308ab60d13b599cb8021a0f264a6191515e93d
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-08-31 16:31:38 +0100

    single responsibility refactor

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,16 +7,27 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.count == 0
+    fail 'No bikes available' if empty?
     @bikes[rand]
   end
 
   def dock(bike)
-    fail 'Dock full' if @bikes.count >= 20
+    fail 'Dock full' if full?
     @bikes << bike
   end
 
   def bike
     @bikes
   end
+
+  private
+
+  def empty?
+    @bikes.empty?
+  end
+
+  def full?
+    @bikes.count >= 20
+  end
+
 end
