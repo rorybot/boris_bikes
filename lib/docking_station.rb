d184f6e10e7c16b15a2be851e0f3800bed9eae01
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-08-31 16:03:47 +0100

    20 bikes, working

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,17 +2,21 @@ require_relative 'bike'
 
 class DockingStation
 
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.count == 0
+    @bikes[rand]
   end
 
   def dock(bike)
-    fail 'Dock full' if @bike
-    @bike = bike
+    fail 'Dock full' if @bikes.count >= 20
+    @bikes << bike
   end
 
   def bike
-    @bike
+    @bikes
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,6 +9,7 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
+
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike}.to raise_error 'No bikes available'
     end
@@ -16,18 +17,20 @@ describe DockingStation do
 
   it 'docks something' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
 
   it 'raises an error when there is already a bike docked' do
-    subject.dock(Bike.new)
+    20.times {subject.dock Bike.new }
     expect { subject.dock Bike.new }.to raise_error 'Dock full'
   end
 
+  describe '#dock' do
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bike).to eq [bike]
+  end
   end
 
   describe '#release_bike' do
@@ -37,4 +40,9 @@ describe DockingStation do
       expect(subject.release_bike).to eq bike
     end
   end
+
+  # it 'raises an error when full' do
+  #      20.times { subject.dock Bike.new }
+  #      expect { subject.dock Bike.new }.to raise_error 'Dock full'
+  # end
 end
